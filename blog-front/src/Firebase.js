3753e87dcc47307ddd555c6deb3053fbafce8a93
secolema <seanclmn1@gmail.com>
2022-05-11 21:57:21 -0400

    fixed firebase config with env

--- blog-front/src/Firebase.js
+++ blog-front/src/Firebase.js
@@ -4,14 +4,22 @@ import "firebase/firestore";
 import "firebase/storage";
 
 const app = firebase.initializeApp({
+  // apiKey: "AIzaSyAqyS83oZ_FyReI6Z3xlloDDQP8j-CG5FA",
+  // authDomain: "blog-2a160.firebaseapp.com",
+  databaseURL: "https://blog-2a160-default-rtdb.firebaseio.com",
+  projectId: "blog-2a160",
+  storageBucket: "blog-2a160.appspot.com",
+  messagingSenderId: "204056153647",
+  appId: "1:204056153647:web:f312f1035c04ab8548c0da",
+  measurementId: "G-CPPZEW89DP",
   apiKey: `${process.env.REACT_APP_APIKEY}`,
-  authDomain: `${process.env.authDomain}`,
-  databaseURL: `${process.env.databaseURL}`,
-  projectId: `${process.env.projectId}`,
-  storageBucket: `${process.env.storageBucket}`,
-  messagingSenderId: `${process.env.messagingSenderId}`,
-  appId: `${process.env.appId}`,
-  measurementId: `${process.env.measurementId}`,
+  authDomain: `${process.env.REACT_APP_AUTHDOMAIN}`,
+  databaseURL: `${process.env.REACT_APP_DATABASEURL}`,
+  projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_STORAGEBUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_APP_ID}`,
+  measurementId: `${process.env.REACT_APP_MEASUREMENT_ID}`,
 });
 
 export const auth = firebase.auth();
