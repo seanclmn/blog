7c4e8ee2100f1b57389dfceb5c7e0b828fddbb71
secolema <seanclmn1@gmail.com>
2022-05-25 20:04:48 -0400

    blog block fix

--- blog-front/src/components/BlogBlock.tsx
+++ blog-front/src/components/BlogBlock.tsx
@@ -1,37 +1,36 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 interface Props {
-  title: string
-  date: string
-  img: string
-  link: string
+  title: string;
+  date: string;
+  img: string;
+  link: string;
 }
 
-function BlogBlock({title, date, img, link}:Props) {
+function BlogBlock({ title, date, img, link }: Props) {
   return (
-    <Link 
+    <Link
       to={`/explore/${link}`}
       className="transition delay-70 
         flex flex-col justify-between w-[100%] max-w-[300px] 
-        h-[300px] pt-[10px] bg-white 
-        rounded-md mx-auto 
+        h-[300px] bg-white 
+        rounded-md border border-solid border-gray-100 border-w-[0.5px] mx-auto 
         hover:shadow-[10px_10px_10px_5px_rgba(0,0,0,0.05)]
         cursor-pointer no-underline text-black
-        ">
-      <img 
-        className="object-cover h-[80%] saturate-[.9] hover:saturate-[1]" 
+        "
+    >
+      <img
+        className="object-cover h-[80%] saturate-[.9] hover:saturate-[1]"
         src={img}
-				loading="lazy"
+        loading="lazy"
       />
-      <div
-        className='px-[20px] pb-[10px]'
-        >
+      <div className="px-[20px] pb-[10px]">
         <p className="font-serif text-gray-500 leading-[5px] text-xs">{date}</p>
 
         <p className="font-seif leading-[0px] text-xl">{title}</p>
       </div>
     </Link>
-  )
+  );
 }
 
-export default BlogBlock
+export default BlogBlock;
--- blog-front/src/routes/Home.tsx
+++ blog-front/src/routes/Home.tsx
@@ -1,74 +1,81 @@
 //@ts-nocheck
 
-import React,{useEffect, useState} from 'react'
-import firebase from "firebase"
-import {doc,onSnapshot} from "firebase/firestore"
-import BlogBlock from "../components/BlogBlock"
-import {Link} from 'react-router-dom'
-import {
-  SimpleGrid,
-  LoadingOverlay,
-  AppShell,
-  Header
-} from "@mantine/core";
+import React, { useEffect, useState } from "react";
+import firebase from "firebase";
+import { doc, onSnapshot } from "firebase/firestore";
+import BlogBlock from "../components/BlogBlock";
+import { Link } from "react-router-dom";
+import { SimpleGrid, LoadingOverlay, AppShell, Header } from "@mantine/core";
 interface Props {}
 
 function Home(props: Props) {
-  const [blogs,setBlogs]=useState([])
-  const [loading,setLoading]=useState(true)
+  const [blogs, setBlogs] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  const ref = firebase.firestore().collection("blogs")
+  const ref = firebase.firestore().collection("blogs");
 
-  function getBlogs(){  
-    ref.onSnapshot((querySnapshot)=>{
-      const items = []
-      querySnapshot.forEach((doc)=>{
-        items.push({data: doc.data(), id: doc.id})
-      })
-      console.log(items)
-      setBlogs(items.sort((a,b)=>new Date(b.data.date)-new Date(a.data.date)))
-    })
+  function getBlogs() {
+    ref.onSnapshot((querySnapshot) => {
+      const items = [];
+      querySnapshot.forEach((doc) => {
+        items.push({ data: doc.data(), id: doc.id });
+      });
+      console.log(items);
+      setBlogs(
+        items.sort((a, b) => new Date(b.data.date) - new Date(a.data.date))
+      );
+    });
   }
 
-  useEffect(()=>{
-    getBlogs()
-    setLoading(false)
-  },[])
+  useEffect(() => {
+    getBlogs();
+    setLoading(false);
+  }, []);
 
-  if(loading || !blogs.length) return <div className="w-[100%] h-[100vh]"><></><LoadingOverlay visible={loading || !blogs.length}/></div>
+  if (loading || !blogs.length)
+    return (
+      <div className="w-[100%] h-[100vh]">
+        <></>
+        <LoadingOverlay visible={loading || !blogs.length} />
+      </div>
+    );
   return (
-
     <AppShell
-    fixed
-    header={
-      <Header height={70} p="xs" className="px-[50px]">
-        <div className='w-[60px]'>
-          <Link to="/" className="cursor-pointer no-underline text-black">
-            <h1 >Blog</h1>
-          </Link>
-        </div>
-      </Header>
-    }
-  >
-    {blogs.length!==0 && <SimpleGrid 
-      className="w-[75%] mx-auto"
-      breakpoints={[
-        { minWidth: 0, cols: 1 },
-        { minWidth: 1000, cols: 2 },
-        { minWidth: 1400, cols: 3 },
-      ]}
-      cols={3}>
-      {blogs.map((blog)=>
-        <BlogBlock 
-          key={blog.id} 
-          title={blog.data.title} 
-          date={blog.data.date} 
-          img={blog.data.image} 
-          link={blog.id}/>
-        )}
-    </SimpleGrid>}
-  </AppShell>
-  )
+      className="trueGray-50"
+      fixed
+      header={
+        <Header height={70} p="xs" className="px-[50px]">
+          <div className="w-[60px]">
+            <Link to="/" className="cursor-pointer no-underline text-black">
+              <h1>Blog</h1>
+            </Link>
+          </div>
+        </Header>
+      }
+    >
+      {blogs.length !== 0 && (
+        <SimpleGrid
+          className="w-[75%] mx-auto"
+          breakpoints={[
+            { minWidth: 0, cols: 1 },
+            { minWidth: 1000, cols: 2 },
+            { minWidth: 1400, cols: 3 },
+          ]}
+          cols={3}
+        >
+          {blogs.map((blog) => (
+            <BlogBlock
+              key={blog.id}
+              title={blog.data.title}
+              date={blog.data.date}
+              img={blog.data.image}
+              link={blog.id}
+            />
+          ))}
+        </SimpleGrid>
+      )}
+    </AppShell>
+  );
 }
 
-export default Home
+export default Home;
